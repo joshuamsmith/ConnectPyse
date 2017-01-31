634297d3e2160ee6bbc1357723e1e2a5de9c3830
Joshua M Smith <saether@gmail.com>
2017-01-30 21:40:43 -0800

    Update cw_company.py with class and param property
    Swapped having the child class pass in methods for assigning it as a property since it's used multiple times.
    Also added a private method to take advantage of the parameters some of the methods use to filter requests.

--- cw_controller.py
+++ cw_controller.py
@@ -7,6 +7,7 @@ class CWController(restapi.Client):
     def __init__(self):
         # self.module_url comes from child
         # self.module comes from child
+        # self._class comes from child
         self.conditions = ''
         self.orderBy = ''
         self.childconditions = ''
@@ -15,34 +16,47 @@ class CWController(restapi.Client):
         self.pageSize = ''
         super().__init__('{}/{}'.format(connectpyse.API_URL, self.module_url))
 
-    def _get(self, child, user_params={}):
-        json_results = getattr(self, self.module).get(user_headers=connectpyse.basic_auth, user_params=user_params)
-        for json in json_results:
-            yield child(json)
+    def _format_user_params(self):
+        user_params = {}
+        for param in ['conditions', 'orderBy', 'childconditions', 'customfieldconditions', 'page', 'pageSize']:
+            user_params[param] = getattr(self, param)
+        return user_params
 
-    def _create(self, child, the_item):  # TODO: Test
+    def _get(self):
+        json_results = getattr(self, self.module).get(user_headers=connectpyse.basic_auth,
+                                                      user_params=self._format_user_params())
+        for json in json_results:
+            yield self._class(json)
+
+    def _create(self, a_object):  # TODO: Test
         # Ideally take the_item and submit that as the user_data
-        dict_post = {'name': the_item.name, 'company': the_item.company, 'type': the_item.type}
-        an_instance = child(getattr(self, self.module).post(user_data=dict_post, user_headers=connectpyse.basic_auth))
+        try:
+            clean_dict = {k: v for k, v in a_object.__dict__.items() if v}
+        except Exception as e:
+            print(repr(e))
+            return False
+        an_instance = self._class(
+            getattr(self, self.module).post(user_data=clean_dict, user_headers=connectpyse.basic_auth))
         return an_instance
 
     def _get_count(self):
-        json_results = getattr(self, self.module).get(the_id='count', user_headers=connectpyse.basic_auth)
+        json_results = getattr(self, self.module).get(the_id='count', user_headers=connectpyse.basic_auth,
+                                                      user_params=self._format_user_params())
         count = json_results['count']
         return count
 
-    def _get_by_id(self, child, item_id):
-        an_instance = child(getattr(self, self.module).get(the_id=item_id, user_headers=connectpyse.basic_auth))
+    def _get_by_id(self, item_id):
+        an_instance = self._class(getattr(self, self.module).get(the_id=item_id, user_headers=connectpyse.basic_auth))
         return an_instance
 
     def _delete_by_id(self, item_id):
         return getattr(self, self.module).delete(the_id=item_id, user_headers=connectpyse.basic_auth)
 
-    def _replace(self, company_id):
+    def _replace(self, company_id):  # TODO: test
         # copy code from _create once tested, use PUT verb
         pass
 
-    def _update(self, child, item_id, key, value):
+    def _update(self, item_id, key, value):
         # build PatchOperation dict
         patch_operation = [{
             'op': 'replace',
@@ -50,11 +64,11 @@ class CWController(restapi.Client):
             'value': value
         }]
         # call Patch method on API
-        an_instance = child(getattr(self, self.module).patch(the_id=item_id, user_data=patch_operation,
-                                                             user_headers=connectpyse.basic_auth))
+        an_instance = self._class(getattr(self, self.module).patch(the_id=item_id, user_data=patch_operation,
+                                                                   user_headers=connectpyse.basic_auth))
         return an_instance
 
-    def _merge(self, item_id, target_id):
+    def _merge(self, item_id, target_id):  # TODO: test
         company_merge = [{
             'toCompanyId': target_id
         }]
