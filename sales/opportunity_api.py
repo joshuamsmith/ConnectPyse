8aa70bd79f85b3796856492405db5f222952f8fa
markciecior <mark@markciecior.com>
2019-02-12 21:29:55 -0600

    Added Opportunity API

---
+++ sales/opportunity_api.py
@@ -0,0 +1,36 @@
+from cw_controller import CWController
+# Class for /system/members
+from sales import opportunity
+
+
+class OpportunityAPI(CWController):
+    def __init__(self):
+        self.module_url = 'sales'
+        self.module = 'opportunity'
+        self._class = opportunity.Opportunity
+        super().__init__()  # instance gets passed to parent object
+
+    def get_opportunity(self):
+        return super()._get()
+
+    def create_opportunity(self, a_type):
+        return super()._create(a_type)
+
+    def get_opportunity_count(self):
+        return super()._get_count()
+
+    def get_opportunity_by_id(self, type_id):
+        return super()._get_by_id(type_id)
+
+    def delete_opportunity_by_id(self, type_id):
+        super()._delete_by_id(type_id)
+
+    def replace_opportunity(self, type_id):
+        pass
+
+    def update_opportunity(self, type_id, key, value):
+        return super()._update(type_id, key, value)
+
+    def merge_opportunity(self, a_type, target_type_id):
+        # return super()._merge(a_type, target_type_id)
+        pass
